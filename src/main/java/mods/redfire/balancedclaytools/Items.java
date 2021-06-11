35392dd7bc1e12515be2a967e2fe3e07dddd2b5d
Redfire <redfire75369@hotmail.com>
2021-06-12 03:46:36 +0800

    Added Clay Sword and Clay Hoe
    Changed Clay Tools to use Black handles instead of Wooden handles
    Added Run Tasks and New Logo to Git
    Added CurseForge Description Source Code

--- src/main/java/mods/redfire/balancedclaytools/Items.java
+++ src/main/java/mods/redfire/balancedclaytools/Items.java
@@ -6,9 +6,7 @@
 
 package mods.redfire.balancedclaytools;
 
-import mods.redfire.balancedclaytools.tools.ClayAxe;
-import mods.redfire.balancedclaytools.tools.ClayPickaxe;
-import mods.redfire.balancedclaytools.tools.ClayShovel;
+import mods.redfire.balancedclaytools.tools.*;
 import net.minecraft.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.RegistryObject;
@@ -21,8 +19,10 @@ public class Items {
 	private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
 
 	public static final RegistryObject<Item> CLAY_AXE = ITEMS.register("clay_axe", ClayAxe::new);
+	public static final RegistryObject<Item> CLAY_HOE = ITEMS.register("clay_hoe", ClayHoe::new);
 	public static final RegistryObject<Item> CLAY_PICKAXE = ITEMS.register("clay_pickaxe", ClayPickaxe::new);
 	public static final RegistryObject<Item> CLAY_SHOVEL = ITEMS.register("clay_shovel", ClayShovel::new);
+	public static final RegistryObject<Item> CLAY_SWORD = ITEMS.register("clay_sword", ClaySword::new);
 
 	public static void register(IEventBus bus) {
 		ITEMS.register(bus);
---
+++ src/main/java/mods/redfire/balancedclaytools/tools/ClayHoe.java
@@ -0,0 +1,124 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package mods.redfire.balancedclaytools.tools;
+
+import com.google.common.collect.Multimap;
+import mods.redfire.balancedclaytools.BalancedClayTools;
+import mods.redfire.balancedclaytools.Utilities;
+import net.minecraft.block.BlockState;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.ai.attributes.Attribute;
+import net.minecraft.entity.ai.attributes.AttributeModifier;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.item.HoeItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUseContext;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.Direction;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.List;
+
+import static mods.redfire.balancedclaytools.Utilities.filterMultimap;
+import static net.minecraft.item.ItemTier.DIAMOND;
+
+public class ClayHoe extends HoeItem {
+	public ClayHoe() {
+		super(DIAMOND, 0, 0.0F, new Properties().durability(0).tab(BalancedClayTools.TAB_CLAY_TOOLS));
+	}
+
+	@SuppressWarnings("deprecation")
+	@Nonnull
+	@Override
+	public ActionResultType useOn(@Nonnull ItemUseContext ctx) {
+		World world = ctx.getLevel();
+		BlockPos blockpos = ctx.getClickedPos();
+		int hook = net.minecraftforge.event.ForgeEventFactory.onHoeUse(ctx);
+		if (hook != 0) return hook > 0 ? ActionResultType.SUCCESS : ActionResultType.FAIL;
+		if (ctx.getClickedFace() != Direction.DOWN && world.isEmptyBlock(blockpos.above())) {
+			BlockState blockstate = world.getBlockState(blockpos).getToolModifiedState(world, blockpos, ctx.getPlayer(), ctx.getItemInHand(), net.minecraftforge.common.ToolType.HOE);
+			if (blockstate != null) {
+				PlayerEntity playerentity = ctx.getPlayer();
+				world.playSound(playerentity, blockpos, SoundEvents.HOE_TILL, SoundCategory.BLOCKS, 1.0F, 1.0F);
+				if (!world.isClientSide) {
+					world.setBlock(blockpos, blockstate, 11);
+				}
+				return ActionResultType.sidedSuccess(world.isClientSide);
+			}
+		}
+
+		return ActionResultType.PASS;
+	}
+
+	@Override
+	public boolean mineBlock(@Nonnull ItemStack stack, @Nonnull World world, @Nonnull BlockState state, @Nonnull BlockPos pos, @Nonnull LivingEntity entity) {
+		Utilities.onBlockDestroyed(stack, world, stack, pos, entity);
+		return true;
+	}
+
+	@Override
+	public boolean isRepairable(@Nonnull ItemStack stack) {
+		return false;
+	}
+
+	@Override
+	public boolean isEnchantable(@Nonnull ItemStack stack) {
+		return false;
+	}
+
+	@Override
+	public boolean canApplyAtEnchantingTable(ItemStack stack, Enchantment enchantment) {
+		return false;
+	}
+
+	@Override
+	public boolean onLeftClickEntity(ItemStack stack, PlayerEntity player, Entity entity) {
+		return Utilities.onLeftClickEntity(stack, player, entity);
+	}
+
+	@Override
+	public boolean isBookEnchantable(ItemStack stack, ItemStack book) {
+		return false;
+	}
+
+	@Override
+	public boolean isFoil(@Nonnull ItemStack stack) {
+		return true;
+	}
+
+	@Override
+	public int getItemEnchantability(ItemStack stack) {
+		return 0;
+	}
+
+	@Override
+	public boolean hurtEnemy(@Nonnull ItemStack stack, @Nonnull LivingEntity target, @Nonnull LivingEntity attacker) {
+		return false;
+	}
+
+	@Nonnull
+	@Override
+	public Multimap<Attribute, AttributeModifier> getDefaultAttributeModifiers(@Nonnull EquipmentSlotType equipmentSlot) {
+		return filterMultimap(super.getDefaultAttributeModifiers(equipmentSlot));
+	}
+
+	@Override
+	public void appendHoverText(@Nonnull ItemStack stack, @Nullable World world, List<ITextComponent> tooltip, @Nonnull ITooltipFlag flag) {
+		tooltip.add(new TranslationTextComponent("item.balancedclaytools.clay_tool.tooltip"));
+	}
+}
